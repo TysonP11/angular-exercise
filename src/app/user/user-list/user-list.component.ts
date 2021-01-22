834d309cbf68a5d7afb8d59afa357090cdf71444
ptson2 <son.phamthai@ifisolution.com>
2021-01-22 15:47:32 +0700

    fixed table layout

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -9,13 +9,40 @@ import { AfterViewChecked, AfterViewInit, Component, Input, OnInit } from '@angu
 export class UserListComponent implements OnInit, AfterViewChecked {
 
   @Input() users : User[]; 
+  formatedUsers = []
 
   constructor() { }
 
+  formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
   ngOnInit(): void {
+    this.formatedUsers = this.users.map((user) => {
+      return {
+        ...user,
+        birthDay: this.formatDate(user.birthDay)
+      }
+    })
   }
   
   ngAfterViewChecked(){
     //console.log(this.users)
+    this.formatedUsers = this.users.map((user) => {
+      return {
+        ...user,
+        birthDay: this.formatDate(user.birthDay)
+      }
+    })
   }
 }
